38c4fc4eff992e015d49e0be07d4cf640c56f3ab
Taka0613 <takaya@uni.minerva.edu>
2024-11-07 22:49:00 -0300

    changed for heroku

--- deliberative_democracy_game/run.py
+++ deliberative_democracy_game/run.py
@@ -1,6 +1,8 @@
-# run.py
 from app import create_app, socketio
+import os
 
 app = create_app()
+
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Bind to the Heroku $PORT
+    socketio.run(app, host="0.0.0.0", port=port)
