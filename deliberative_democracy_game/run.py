6d68a55760795a557e3a3dd09a8c50dbd205e730
Taka0613 <takaya@uni.minerva.edu>
2024-11-07 22:33:45 -0300

    Prepare app for Heroku deployment

--- deliberative_democracy_game/run.py
+++ deliberative_democracy_game/run.py
@@ -1,11 +1,6 @@
 # run.py
-
 from app import create_app, socketio
 
-# Create the Flask app instance
 app = create_app()
-
-# Main entry point for Heroku
 if __name__ == "__main__":
-    # Use eventlet or gevent for production if you're using SocketIO
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    socketio.run(app, debug=True)
